03b72da142e3bb099030cfb2315a1abf6887e748
aaxel242 <ortegagonzalezaxel@gmail.com>
2025-03-24 20:07:28 +0100

    POR HACER: acabar el bottomnavigationview, hacer los fragments de chat y perfil y hacer que funcione

--- app/src/main/java/com/example/cercleculturalandroid/MainActivity.kt
+++ app/src/main/java/com/example/cercleculturalandroid/MainActivity.kt
@@ -5,6 +5,8 @@ import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,12 +17,19 @@ class MainActivity : AppCompatActivity() {
         spinner.gravity = android.view.Gravity.CENTER
 
         val adapter = ArrayAdapter.createFromResource(
-            this,
-            R.array.opciones_menu,
-            R.layout.spinner_item  // Usa el TextView personalizado
+            this, R.array.opciones_menu, R.layout.spinner_item
                                                      ).also { adapter ->
-            adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)  // Layout para el dropdown
+            adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
         }
         spinner.adapter = adapter
+
+        val rvEvents = findViewById<RecyclerView>(R.id.rvEvents)
+        rvEvents.layoutManager = LinearLayoutManager(this)
+
+        //Adapter de eventos
+        val eventsAdapter = EventosAdapter()
+
+
+
     }
 }
