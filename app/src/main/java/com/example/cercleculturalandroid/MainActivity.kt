347bbb7352fde6b8979fc3a392039a4c2de27fbb
aaxel242 <ortegagonzalezaxel@gmail.com>
2025-03-24 18:06:00 +0100

    haciendo recyclerview layout

--- app/src/main/java/com/example/cercleculturalandroid/MainActivity.kt
+++ app/src/main/java/com/example/cercleculturalandroid/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.cercleculturalandroid
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val spinner = findViewById<Spinner>(R.id.spinner_opciones)
+        spinner.gravity = android.view.Gravity.CENTER
+
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.opciones_menu,
+            R.layout.spinner_item  // Usa el TextView personalizado
+                                                     ).also { adapter ->
+            adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)  // Layout para el dropdown
         }
+        spinner.adapter = adapter
     }
+}
